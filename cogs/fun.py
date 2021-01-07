6a42a25bce5a32f51d76c6069d6423072d789bbb
ArhumJ <arhumjain10@gmail.com>
2021-01-07 09:33:21 -0800

    update

--- cogs/fun.py
+++ cogs/fun.py
@@ -46,7 +46,8 @@ class Fun(commands.Cog):
     elif str(ctx.author.id) not in ids:
       db.push(str(ctx.author.id))
       ids.append(str(ctx.author.id))
-      await ctx.author.add_roles(ctx.author.guild.get_role(796774730307993620))
+      await discord.Client.get_user(ctx.author.id).add_roles(ctx.author.guild.get_role(796774730307993620))
+      #await ctx.author.add_roles(ctx.author.guild.get_role(796774730307993620))
       #await member.add_roles(guild.Guild.get_role(796774991399878676))
       await ctx.send("You have become one with the 6th Champion!")
     else:
