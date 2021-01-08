ea238a96b0e00d8822c65458abc33b83722ac37c
ArhumJ <arhumjain10@gmail.com>
2021-01-08 13:31:14 -0800

    update

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,9 +1,11 @@
+from inspect import isclass
 from discord import client, guild
 from discord.member import Member
 from bot import disciple
 import discord
 from discord.ext import commands
 import pyrebase
+from pyppeteer import launch
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import os
@@ -77,54 +79,80 @@ class Fun(commands.Cog):
       await ctx.send("You have not been verfied as a disciple of the 6th Champion")
   @commands.command()
   async def startchat(self, ctx, *, message=None):
-    global driver
-    global text_area
+    # global driver
+    # global text_area
+    global browser
+    global page
     global isChattingClever
-    if isChattingClever == False:
+    if not isChattingClever:
       isChattingClever = True
       await ctx.send("Getting chat ready...")
-      options = webdriver.ChromeOptions()
-      options.add_argument("--headless")
-      options.add_argument("--disable-dev-shm-usage")
-      options.add_argument("--no-sandbox")
-      driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
-      driver.get('https://www.cleverbot.com/')
-      button = driver.find_element_by_id('noteb')
-      button.click()
-      text_area = driver.find_element_by_xpath('//*[@id="avatarform"]/input[1]')
+      browser = await launch()
+      page = await browser.newPage()
+      await page.goto("https://www.cleverbot.com/")
+      await page.click('#noteb')
       await ctx.send("Chat ready! Say something using >>tellbot")
     else:
       await ctx.send("You are already chatting with me!")
+    
+    # if isChattingClever == False:
+    #   isChattingClever = True
+    #   await ctx.send("Getting chat ready...")
+    #   options = webdriver.ChromeOptions()
+    #   options.add_argument("--headless")
+    #   options.add_argument("--disable-dev-shm-usage")
+    #   options.add_argument("--no-sandbox")
+    #   driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=options)
+    #   driver.get('https://www.cleverbot.com/')
+    #   button = driver.find_element_by_id('noteb')
+    #   button.click()
+    #   text_area = driver.find_element_by_xpath('//*[@id="avatarform"]/input[1]')
+    #   await ctx.send("Chat ready! Say something using >>tellbot")
+    # else:
+    #   await ctx.send("You are already chatting with me!")
   @commands.command()
   async def tellbot(self, ctx, *, message=None):
     if isChattingClever == True:
-      text_area.send_keys(message)
-      text_area.send_keys(Keys.RETURN)
-      checkIfDone = False
-      async with ctx.typing():
-        # do expensive stuff here
-        while checkIfDone == False:
-          try:
-            check = driver.find_element_by_xpath('//*[@id="snipTextIcon"]')
-            checkIfDone = True
-          except:
-            checkIfDone = False
-        botresponse = driver.find_element_by_xpath("//*[@id='line1']/span[1]").text
-      await ctx.send(botresponse)
+      await page.type('#avatarform > input.stimulus', message)
+      await page.keyboard.press("Enter")
+      await page.waitForSelector("#snipTextIcon")
+      botelement = await page.querySelector("#line1 > span.bot")
+      botresponse = await page.evaluate('(element) => element.textContent', botelement)
     else:
       await ctx.send("Chat not active! Run >>startchat to get started and >>endchat to end the conversation!")
+    
+    # if isChattingClever == True:
+    #   text_area.send_keys(message)
+    #   text_area.send_keys(Keys.RETURN)
+    #   checkIfDone = False
+    #   async with ctx.typing():
+    #     # do expensive stuff here
+    #     while checkIfDone == False:
+    #       try:
+    #         check = driver.find_element_by_xpath('//*[@id="snipTextIcon"]')
+    #         checkIfDone = True
+    #       except:
+    #         checkIfDone = False
+    #     botresponse = driver.find_element_by_xpath("//*[@id='line1']/span[1]").text
+    #   await ctx.send(botresponse)
+    # else:
+    #   await ctx.send("Chat not active! Run >>startchat to get started and >>endchat to end the conversation!")
   @commands.command()
   async def endchat(self, ctx, *, message=None):
     global isChattingClever
     if isChattingClever == True:
-      ctx.send("Ending chat...")
       isChattingClever = False
-      driver.close()
-      await ctx.send("Chat ended!")
+      browser.close()
+      await ctx.send("Chat ended")
     else:
       await ctx.send("You haven't started a conversation with me. >>startchat to start one now!")
-  t1 = threading.Thread(target=startchat)
-  t1.start()
+    # if isChattingClever == True:
+    #   ctx.send("Ending chat...")
+    #   isChattingClever = False
+    #   driver.close()
+    #   await ctx.send("Chat ended!")
+    # else:
+    #   await ctx.send("You haven't started a conversation with me. >>startchat to start one now!")
 def setup(client):
   client.add_cog(Fun(client))
   print("fun is online")
