d6ea87f1dfd1006940f7f935a9c14b63dc7f8f83
ArhumJ <arhumjain10@gmail.com>
2021-01-07 16:01:16 -0800

    update

--- cogs/fun.py
+++ cogs/fun.py
@@ -26,9 +26,9 @@ dbids = db.get()
 ids = []
 for i in dbids.each():
   ids.append(i.val())
-isChattingClever = False
-class Fun(commands.Cog):
 
+class Fun(commands.Cog):
+  isChattingClever = False
   def __init__(self,client):
 
     self.client = client
