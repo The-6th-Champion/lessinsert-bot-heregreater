bade6d880cd3bb1bf4603a888e19545e9f5801c9
sid-ms <67034744+sid-ms@users.noreply.github.com>
2021-01-15 19:07:41 -0800

    ^

--- bot.py
+++ bot.py
@@ -17,12 +17,12 @@ def is_it_me(ctx):
 @client.event
 async def on_guild_join(guild):
     channel = client.get_channel(792842806291988481)
-    embed = discord.Embed(title="New Server Joined!!!!", description=f"<Insert Bot Here> has Joined {guild.name}.\nThe ID is {guild.id}.\nIt is owned by {guild.owner.mention}.\nIt's membercount is {guild.member_count}.", color = discord.Color(0x0f0))
+    embed = discord.Embed(title="New Server Joined!!!!", description=f"<Insert Bot Here> has Joined {guild.name}.\nThe ID is {guild.id}.\nIt is owned by {guild.owner.mention}.\nIt's membercount is {guild.member_count}.", color = discord.Color(0x00ff00))
     await channel.send(embed=embed)
 @client.event
 async def on_guild_remove(guild):
     channel = client.get_channel(792842806291988481)
-    embed = discord.Embed(title="Server Left <a:PensiveWobble:799822678386147388>", description=f"<Insert Bot Here> has left {guild.name}.\nThe ID is {guild.id}.\nIt is owned by {guild.owner.mention}.\nIt's membercount is {guild.member_count}.", color = discord.Color(0xf00))
+    embed = discord.Embed(title="Server Left <a:PensiveWobble:799822678386147388>", description=f"<Insert Bot Here> has left {guild.name}.\nThe ID is {guild.id}.\nIt is owned by {guild.owner.mention}.\nIt's membercount is {guild.member_count}.", color = discord.Color(0xff0000))
     await channel.send(embed=embed)
 #help command
 @client.group(name="help", invoke_without_command=True)
