0cb27c0e01a276fbf548571176216a9bc0204ded
sid-ms <67034744+sid-ms@users.noreply.github.com>
2021-02-11 08:54:50 -0800

    maybe now/

--- bot.py
+++ bot.py
@@ -19,18 +19,20 @@ configvars = {
     "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
     "client_x509_cert_url": os.environ.get("client_x509_cert_url")
 }
-print({
-    "type": "service_account",
-    "project_id": os.environ.get('project_id'),
-    "private_key_id": os.environ.get('private_key_id'),
-    "private_key": os.environ.get('private_key').replace('\\n', '\n'),
-    "client_email": os.environ.get('client_email'),
-    "client_id": os.environ.get('client_id'),
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://accounts.google.com/o/oauth2/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": os.environ.get('client_x509_cert_url')
-        })
+#print({
+#    "type": "service_account",
+ #   "project_id": os.environ.get('project_id'),
+  #  "private_key_id": os.environ.get('private_key_id'),
+   # "private_key": os.environ.get('private_key').replace('\\n', '\n'),
+    #"client_email": os.environ.get('client_email'),
+    #"client_id": os.environ.get('client_id'),
+    #"auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    #"token_uri": "https://accounts.google.com/o/oauth2/token",
+    #"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    #"client_x509_cert_url": os.environ.get('client_x509_cert_url')
+#    })
+json.dump(configvars, open('stuffs.json', 'w'))
+cred = credentials.Certificate('stuffs.json')
 default_app = firebase_admin.initialize_app(cred)
 intents = discord.Intents.default()
 intents.guilds = True
@@ -38,7 +40,7 @@ intents.members = True
 client = commands.Bot(command_prefix=commands.when_mentioned_or('>>'), intents = intents)
 client.remove_command("help")
 TOKEN = TOKEN = os.environ.get("TOKEN")
-json.dump(configvars, open('stuffs.json', 'w'))
+
 
 #cred = credentials.Certificate('stuffs.json')
 #default_app = firebase_admin.initialize_app(cred)
