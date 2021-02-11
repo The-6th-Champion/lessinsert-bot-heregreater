307698c56b7b4e020c7258c71ab39039ac8120f5
sid-ms <67034744+sid-ms@users.noreply.github.com>
2021-02-10 20:14:48 -0800

    im out of names....this was a newline problem

--- bot.py
+++ bot.py
@@ -9,15 +9,15 @@ import json
 
 
 configvars = {
-  "type": "service_account",
-  "project_id": os.environ.get("project_id"),
-  "private_key": os.environ.get("private_key"),
-  "client_email": os.environ.get("client_email"),
-  "client_id": os.environ.get("client_id"),
-  "auth_uri": os.environ.get("auth_uri"),
-  "token_uri": os.environ.get("token_uri"),
-  "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
-  "client_x509_cert_url": os.environ.get("client_x509_cert_url")
+    "type": "service_account",
+    "project_id": os.environ.get("project_id"),
+    "private_key": os.environ.get('PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.environ.get("client_email"),
+    "client_id": os.environ.get("client_id"), 
+    "auth_uri": os.environ.get("auth_uri"),
+    "token_uri": os.environ.get("token_uri"),
+    "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
+    "client_x509_cert_url": os.environ.get("client_x509_cert_url")
 }
 
 intents = discord.Intents.default()
