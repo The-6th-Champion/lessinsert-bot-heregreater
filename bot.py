0a51b9c3d9d05a692ea1c71bef18d03cb7ce4f4e
sid-ms <67034744+sid-ms@users.noreply.github.com>
2021-02-10 20:03:09 -0800

    hm

--- bot.py
+++ bot.py
@@ -13,6 +13,11 @@ configvars = {
   "project_id": os.environ.get("project_id"),
   "private_key": os.environ.get("private_key"),
   "client_email": os.environ.get("client_email"),
+  "client_id": os.environ.get("client_id"),
+  "auth_uri": os.environ.get("auth_uri"),
+  "token_uri": os.environ.get("token_uri"),
+  "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
+  "client_x509_cert_url": os.environ.get("client_x509_cert_url")
 }
 
 intents = discord.Intents.default()
