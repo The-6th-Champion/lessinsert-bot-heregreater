3d5e4a0eb89adce544852738a29ae4bea777fb82
sid-ms <67034744+sid-ms@users.noreply.github.com>
2021-02-11 11:08:16 -0800

    toggling for say

--- bot.py
+++ bot.py
@@ -19,18 +19,7 @@ configvars = {
     "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
     "client_x509_cert_url": os.environ.get("client_x509_cert_url")
 }
-#print({
-#    "type": "service_account",
- #   "project_id": os.environ.get('project_id'),
-  #  "private_key_id": os.environ.get('private_key_id'),
-   # "private_key": os.environ.get('private_key').replace('\\n', '\n'),
-    #"client_email": os.environ.get('client_email'),
-    #"client_id": os.environ.get('client_id'),
-    #"auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    #"token_uri": "https://accounts.google.com/o/oauth2/token",
-    #"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    #"client_x509_cert_url": os.environ.get('client_x509_cert_url')
-#    })
+
 json.dump(configvars, open('stuffs.json', 'w'))
 cred = credentials.Certificate('stuffs.json')
 default_app = firebase_admin.initialize_app(cred)
@@ -62,8 +51,7 @@ class MyHelpCommand(commands.MinimalHelpCommand):
         for page in self.paginator.pages:
             e.description += page
         await destination.send(embed=e)
-        print("help")
-
+        
 client.help_command = MyHelpCommand()
 
 
--- cogs/say.py
+++ cogs/say.py
@@ -1,59 +1,88 @@
 import discord
 import asyncio
 from discord.ext import commands
+import firestore_admin
 
 class Say(commands.Cog):
 
   def __init__(self,client):
 
     self.client = client
+  db = firestore_admin.firestore.Client()
+
+  @commands.command(name = "nosay" description = "This can toggle the auto-responses that are triggered from certain phrases like\n \'why\' and \'why not\'\n and\n \'Hello\' and \'Hi <name>")
+  @commands.has_permissions(administrator = True)
+  async def nosay(self, ctx):
+    ref = db.collection("guilds").doc(message.guild.id)
+    doc = ref.get()
+    data = doc.to_dict()
+    if data.toggle_say == True:
+      doc.update({
+        "toggle_say" : False
+      })
+    elif data.toggle_say == False:
+      doc.update({
+        "toggle_say" : True
+      }) 
+    else:
+      doc.update({
+        "toggle_say" : False
+      })
+
   @commands.Cog.listener()
   async def on_message(self, message):
-    if message.author == self.client.user:
-      pass
-    elif message.author != self.client.user:
-      message1 = message.content
-      message1 = message1.lower()
-      #await message.channel.send(message1)
-      hello1 = "hello" in message1
-      hello2 = "hello there" in message1
-      e1 = "é" in message1
-      inh1 = "<insert name here>" in message1
-      link1="link" in message1
-      #why = "why" in message1
-      #howru = "how are you" in message1
-      #howru2 = "hru" in message1
-      #hbu = "hbu" in message1
-      #hbu2 = "how about you" in message1
-      bowdown = "bow down to the 6th champion!" in message1
-      praisechamp = "praise the 6th champion!" in message1
-      bowdownl = "bow down to link" in message1
-      ytea = "yorkshire tea" in message1
-      if hello1 ==True and hello2 ==True:
-        await message.channel.send(f"*General Kenobi*")
-      elif hello1 ==True and hello2 != True:
-        await message.channel.send(f"Hi {message.author.name}")
-      elif e1 == True and message.author != 780928781858373672:
-        await message.channel.send(f"é")
-      elif inh1 ==True:
-        await message.channel.send("<insert name here> is __**The Creator**__. He is also known as The 6th Champion, Da6thChamp,  and <The 6th Champion>. He developed me, and also has a support server. He is amazing. Bow down to him :person_bowing:!!!")
-      #elif why == True:
-        #await message.channel.send("why not?")
-      elif link1 ==True and bowdownl ==False:
-        await message.channel.send("The hero of hyrule. He is resurrected a lot.")
-      #elif howru ==True or howru2==True:
-        #await message.channel.send("I am pretty good, hbu?")
-      #elif hbu ==True or hbu2==True:
-        #await message.channel.send("I am pretty good.")
-      #things have been commented out to decrease annoying ness
-      elif bowdown == True or praisechamp == True:
-        await message.channel.send("It must be done!")
-        await message.channel.send(file=discord.File('./gifs/praisechampion.gif'))
-      elif bowdownl ==True and link1 == True:
-        await message.channel.send("smh no")
-      elif ytea == True:
-        await message.channel.send("Yorkshire Tea is amazing")
-        await message.channel.send("<:yorkshire_1:798737240313561128><:yorkshire_2:798737240502435851>\n<:yorkshire_3:798737240112889888><:yorkshire_4:798737240276598814>")
+    
+    ref = db.collection("guilds").doc(message.guild.id)
+    doc = ref.get()
+    data = doc.to_dict()
+    if data.toggle_say == True:
+      #send message
+      if message.author == self.client.user:
+        pass
+      elif message.author != self.client.user:
+        message1 = message.content
+        message1 = message1.lower()
+        #await message.channel.send(message1)
+        hello1 = "hello" in message1
+        hello2 = "hello there" in message1
+        e1 = "é" in message1
+        inh1 = "<insert name here>" in message1
+        link1="link" in message1
+        #why = "why" in message1
+        #howru = "how are you" in message1
+        #howru2 = "hru" in message1
+        #hbu = "hbu" in message1
+        #hbu2 = "how about you" in message1
+        bowdown = "bow down to the 6th champion!" in message1
+        praisechamp = "praise the 6th champion!" in message1
+        bowdownl = "bow down to link" in message1
+        ytea = "yorkshire tea" in message1
+        if hello1 ==True and hello2 ==True:
+          await message.channel.send(f"*General Kenobi*")
+        elif hello1 ==True and hello2 != True:
+          await message.channel.send(f"Hi {message.author.name}")
+        elif e1 == True and message.author != 780928781858373672:
+          await message.channel.send(f"é")
+        elif inh1 ==True:
+          await message.channel.send("<insert name here> is __**The Creator**__. He is also known as The 6th Champion, Da6thChamp,  and <The 6th Champion>. He developed me, and also has a support server. He is amazing. Bow down to him :person_bowing:!!!")
+        #elif why == True:
+          #await message.channel.send("why not?")
+        elif link1 ==True and bowdownl ==False:
+          await message.channel.send("The hero of hyrule. He is resurrected a lot.")
+        #elif howru ==True or howru2==True:
+          #await message.channel.send("I am pretty good, hbu?")
+        #elif hbu ==True or hbu2==True:
+          #await message.channel.send("I am pretty good.")
+        #things have been commented out to decrease annoying ness
+        elif bowdown == True or praisechamp == True:
+          await message.channel.send("It must be done!")
+          await message.channel.send(file=discord.File('./gifs/praisechampion.gif'))
+        elif bowdownl ==True and link1 == True:
+          await message.channel.send("smh no")
+        elif ytea == True:
+          await message.channel.send("Yorkshire Tea is amazing")
+          await message.channel.send("<:yorkshire_1:798737240313561128><:yorkshire_2:798737240502435851>\n<:yorkshire_3:798737240112889888><:yorkshire_4:798737240276598814>")
+    else:
       pass
   @commands.command(description="message from the Creator about the bots chatting ability")
   async def sayinfo(self,ctx):
