67ca1ed53e73ddfbb4efeaf1775ec75e69aa1960
sid-ms <67034744+sid-ms@users.noreply.github.com>
2021-02-10 20:34:28 -0800

    im died

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ cred = credentials.Certificate({
               "auth_uri": "https://accounts.google.com/o/oauth2/auth",
               "token_uri": "https://accounts.google.com/o/oauth2/token",
               "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-              "client_x509_cert_url": os.environ.get('client_cert_url')
+              "client_x509_cert_url": os.environ.get('client_x509_cert_url')
         })
 default_app = firebase_admin.initialize_app(cred)
 intents = discord.Intents.default()
