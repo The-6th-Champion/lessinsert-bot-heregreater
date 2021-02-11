102aa78820cb2166e548e4f4c87b5fdfc5ac4b9e
sid-ms <67034744+sid-ms@users.noreply.github.com>
2021-02-10 19:25:52 -0800

    sigh

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import json
 
 
 configvars = {
-  "type": os.environ.get("type"),
+  "type": "service_account",
   "project_id": os.environ.get("project_id"),
   "private_key_id": os.environ.get("private_key_id"),
   "private_key": os.environ.get("private_key"),
